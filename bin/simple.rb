c8d9c6b1475b91ec1e4d4d7bee2546738152d8e9
Zhenya Telyukov <telyukov@gmail.com>
2017-04-10 17:40:29 +0300

    first commit

---
+++ bin/simple.rb
@@ -0,0 +1,16 @@
+################################################################################
+#   filename.rb
+#   
+#   Purpose
+#   
+#   Details
+#   
+#   xx.xx.2014  ZT Created
+#   xx.xx.2014  Last update
+#
+################################################################################
+require 'SIMPLE'   # lib/simple.rb
+
+# Initial Test
+s = Simple::Sample.new
+s.say_hello
---
+++ lib/simple.rb
@@ -0,0 +1,10 @@
+require "simple/version"
+
+module Simple
+  # Test sample
+  class Sample
+    def say_hello
+      puts 'Hello! Just first test'
+    end
+  end
+end
